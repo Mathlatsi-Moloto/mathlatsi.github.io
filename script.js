c16adaa167e6cfead6f50dd31c771e1dc04cebf5
Mathlatsi Moloto <mathlatsi72@gmail.com>
2020-10-08 17:26:35 +0200

    Third commit

---
+++ script.js
@@ -0,0 +1,20 @@
+
+window.onload = function()
+{
+    let progress = document.getElementById("scrollBar");
+    let toolHeight = document.body.scrollHeight - window.innerHeight;
+    window.onscroll = function()
+    {
+        let progressHeight
+    }
+}
+/* on click event*/
+/* scroll navihation*/
+/*set scroll postion*/
+$(document).ready(function(){
+    $('.menu-toggler').on('click',function(){
+        $(this).togglerClass('open');
+        $('.top-nav').togglerClass('open');
+
+    });
+});
